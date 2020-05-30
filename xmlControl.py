b9549c8289468065ec9b646e4d4eab32d029f0a5
asdssp <62279049+asdssp@users.noreply.github.com>
2020-05-31 00:54:22 +0900

    Update xmlControl.py

--- xmlControl.py
+++ xmlControl.py
@@ -37,7 +37,7 @@ import http.client
 class xmlControl:
     def loadXmlFromOpenAPI(params):
         client_id = "10000333"
-        client_secret = "ab2ac2c04da54c3d"
+        client_secret = ""
         conn = http.client.HTTPConnection("safe182.go.kr")
         #conn.set_debuglevel(1) #debug mode �¦ㅼ��
         #headers = {"esntlId": client_id, "authKey": client_secret, "rowSize":"8"}
