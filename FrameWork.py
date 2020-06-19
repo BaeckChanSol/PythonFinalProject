a0ed79dc57227febaab98f8ea72cd1df48786e85
asdssp <62279049+asdssp@users.noreply.github.com>
2020-06-19 23:04:27 +0900

    20/06/19
    프레임 워크 수정

--- FrameWork.py
+++ FrameWork.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import font
 from DataBase import  *
+from GraphData import *
 from Gmap import *
 from tkinter import messagebox
 import webbrowser
@@ -9,11 +10,11 @@ import base64
 from PIL import Image,ImageTk
 from io import BytesIO
 from Gmail import *
-
+import spam
 class FrameWork:
     def __init__(self):
         # 데이터 베이스 생성
-        self.param = ""
+        self.param = "&rowSize=8"
         self.index = 1
         #window 생성
         self.window = Tk()
@@ -22,6 +23,9 @@ class FrameWork:
         self.window.configure(bg="silver")
         self.fontstyle = font.Font(self.window, size = 12, weight='bold', family = 'Consolas')
         self.fontstyle2 = font.Font(self.window, size=10, weight='bold', family='Consolas')
+        self.fontstyle3 = font.Font(self.window, size=8, weight='bold', family='Consolas')
+        self.ectType = {'010': "정상", '020': "가출인", '040': "시설보호무연고자", '060': '지적장애인', '061': '지적장애인(18세미만)', \
+                        '062': '지적장애인(18세이상)', '070': '치매질환자', '080': '불상(기타)'}
         #전체 버튼 생성 함수
         self.setupButton()
         #전체 글자 출력 함수
@@ -37,12 +41,32 @@ class FrameWork:
         self.photo = Canvas(self.window, width=200,height=200, bg='white')
         self.photo.place(x=10,y=10)
 
-        #지도용 캠버스
-        self.MapCanvas = Canvas(self.window,width=340,height=370,bg="white")
-        self.MapCanvas.place(x=440,y=10)
-
+        #그래프 캠버스
+        self.GraphCanvas = Canvas(self.window,width=340,height=370,bg="white")
+        self.GraphCanvas.place(x=440,y=10)
+        self.DownloadGraphData()
+        self.DrawGraphCanvas()
         self.window.mainloop()
 
+    def DownloadGraphData(self):
+        self.GraphData = {};
+        self.GraphData["010"] = (GraphData(1, "&rowSize=1&writngTrgetDscds=" + "010").getPage())
+        self.GraphData["020"] =( GraphData(1, "&rowSize=1&writngTrgetDscds=" + "020").getPage())
+        self.GraphData["040"] = (GraphData(1, "&rowSize=1&writngTrgetDscds=" + "040").getPage())
+        self.GraphData["060"] = (GraphData(1, "&rowSize=1&writngTrgetDscds=" + "060").getPage())
+        self.GraphData["061"] = (GraphData(1, "&rowSize=1&writngTrgetDscds=" + "061").getPage())
+        self.GraphData["062"] = (GraphData(1, "&rowSize=1&writngTrgetDscds=" + "062").getPage())
+        self.GraphData["070"] = (GraphData(1, "&rowSize=1&writngTrgetDscds=" + "070").getPage())
+        self.GraphData["080"] = (GraphData(1, "&rowSize=1&writngTrgetDscds=" + "080").getPage())
+
+    def DrawGraphCanvas(self):
+        maxCount = max(self.GraphData.values())
+        dataCode = ["010", "020", "040", "060", "061", "062", "070", "080"]
+        for i in range(8):
+            self.GraphCanvas.create_rectangle(40 * i + 20, 340, 40 * i + 45, 370 - (30+300 * self.GraphData[dataCode[i]] / maxCount),fill='black')
+            self.GraphCanvas.create_text(40 * i + 32 ,  370 - (40+300 * self.GraphData[dataCode[i]] / maxCount)\
+                                         , text = str(self.GraphData[dataCode[i]]), font=self.fontstyle3)
+            self.GraphCanvas.create_text(40 * i + 32, 350, text=dataCode[i],font=self.fontstyle)
     def setupListBox(self, num):
         #리스트 생성시 정보 불러오기
         self.listBox = Listbox(self.window, width=58, height=8, font=self.fontstyle2)
@@ -91,11 +115,25 @@ class FrameWork:
                                    command=self.pressedRenewal)
         self.PageUpButton.place(x=160, y=400)
 
+        self.DetailInfo = Button(self.window, text="상세", width=8, height=1, font=self.fontstyle2, command=self.pressedInfo)
+        self.DetailInfo.place(x=580, y=410)
+
+    def pressedInfo(self):
+        self.miniWindow2 = Tk()
+        fontstyle = font.Font(self.miniWindow2, size=10, weight='bold', family='Consolas')
+        dataCode = ["010", "020", "040", "060", "061", "062", "070", "080"]
+        for i in range(8):
+            Label(self.miniWindow2,width = 20,  text=dataCode[i] + " : " + self.ectType[dataCode[i]], font = fontstyle, anchor=W).pack()
+        Button(self.miniWindow2, text="닫기", width=8, height=1, font=fontstyle, command=self.DestroyMini).pack()
+        self.miniWindow2.mainloop()
+    def DestroyMini(self):
+        self.miniWindow2.destroy()
 
     def setupLabel(self):
         #검색한 결과 라벨
         self.LResultName = Label(text="이름: ", font = self.fontstyle, bg='silver')
         self.LResultName.place(x=220,y=10)
+
 
         self.LResultGender = Label(text="성별: ", font=self.fontstyle, bg='silver')
         self.LResultGender.place(x=220, y=55)
@@ -109,11 +147,11 @@ class FrameWork:
         self.LResultCurrentAge = Label(text="현재 나이: ", font=self.fontstyle, bg='silver')
         self.LResultCurrentAge.place(x=220, y=185)
 
-        self.LResultRemarks = Label(text="비고 사항: ",font=self.fontstyle2,bg='silver')
+        self.LResultRemarks = Label(text="비고: ",font=self.fontstyle3,bg='silver')
         self.LResultRemarks.place(x=10,y=220)
 
-        self.LResultPoint = Label(text="그래프", font=self.fontstyle, bg='silver', anchor="center")
-        self.LResultPoint.place(x=590, y=390)
+        self.LResultPoint = Label(text="각 실종자 분류 별 그래프", font=self.fontstyle, bg='silver', anchor="center")
+        self.LResultPoint.place(x=525, y=390)
 
         #검색하는 곳 라벨
         self.LSearchName = Label(text = "이름:", width = 8, height = 1, font = self.fontstyle, bg='silver')
@@ -180,7 +218,7 @@ class FrameWork:
             self.message = ["이름 : " + str(person["name"]), "성 : " + str(person["gender"]),
                       "나이 : " + str(person["age"]) + "->" + str(person["ageNow"])
                       ,"지역: "+ str(person["occrAdres"]), "발생 일시 : " + person["occrde"] ,
-                            "기타 사항 : "+ self.ectType(person["etc"])]
+                            "기타 사항 : "+ self.ectType[person["etc"]]]
             with open("picture.gif","wb") as f:
                 f.write(base64.b64decode(person["photo"]))
             print(self.message)
@@ -189,11 +227,12 @@ class FrameWork:
             return
 
         self.miniWindow = Tk()
+        fontstyle = font.Font(self.miniWindow, size=10, weight='bold', family='Consolas')
         Label(self.miniWindow,text="이메일").grid(row=0,column=0)
         self.emailstr = StringVar()
         self.miniEntry = Entry(self.miniWindow,textvariable=self.emailstr)
         self.miniEntry.grid(row=0,column=1)
-        Button(self.miniWindow, text="데이터 전송", command=self.PressedsendMail).grid(row=0,column=2)
+        Button(self.miniWindow, text="데이터 전송", font=fontstyle, command=self.PressedsendMail).grid(row=0,column=2)
         self.miniWindow.mainloop()
 
     def PressedsendMail(self):
@@ -219,7 +258,7 @@ class FrameWork:
             data += "이름 : " +person["name"] + "\t"
             data += "성별 : " +person["gender"] + "\t"
             data += "주소 : " +person["occrAdres"]  + "\t"
-            data += "분류 : " +self.ectType(person["etc"]) + "\t"
+            data += "분류 : " +self.ectType[person["etc"]] + "\t"
             data += "발생일시 : " +person["occrde"] + "\t"
             data += "현재나이 ; " +person["ageNow"] + "\t"
             data += "당시나이 : " +person["age"] + "\t"
@@ -242,7 +281,7 @@ class FrameWork:
             return
 
     def pressedSearch(self):
-        self.param = ""
+        self.param = "&rowSize=8"
         name = self.name_var.get()
         if name != "":
             self.param += "&nm="+parse.quote(name)
@@ -278,25 +317,11 @@ class FrameWork:
         self.index += 1
         self.setupListBox(self.index)
 
-    def ectType(self, person):
-        #비고사항 구분
-        if person == '010':
-            return "정상"
-        if person == '020':
-            return "가출인"
-        if person == '040':
-            return "시설보호무연고자"
-        if person == '060':
-            return "지적장애인"
-        if person == '061':
-            return "지적장애인(18세미만)"
-        if person == '062':
-            return "지적장애인(18세이상)"
-        if person == '070':
-            return "치매질환자"
-        if person == '080':
-            return "불상(기타)"
-        return ""
+
+
+
+
+
 
 
     def pressedRenewal(self):
@@ -305,19 +330,20 @@ class FrameWork:
             person = self.data.getData(self.listBox.curselection()[0])
             self.LResultName.configure(text= "이름: " + person["name"])
             self.LResultGender.configure(text="성별: " + person["gender"])
-            self.LResultRemarks.configure(text= "비고 사항: " + person["occrAdres"] + "/" + self.ectType(person["etc"]))
+            self.LResultRemarks.configure(text= "비고: " + person["occrAdres"] + "/ " + self.ectType[person["etc"]]\
+                                          + " / " + spam.calc(person["occrde"]) + " 째 실종")
             self.LResultTime.configure(text= "발생 일시: " + person["occrde"])
             self.LResultCurrentAge.configure(text="현재 나이: " + person["ageNow"])
             self.LResultAge.configure(text="당시 나이: " + person["age"])
-
-
-            img = ImageTk.PhotoImage(Image.open(BytesIO(base64.b64decode(person['photo']))))
-            self.photo.delete("all")
-            self.photo.create_image(100,100,image=img)
-            self.photo.image = img
         except:
             messagebox.showinfo(title="갱신 안내", message="갱신하고자 하는 데이터를 선택해주세요.")
-
-
+        try:
+            img = ImageTk.PhotoImage(Image.open(BytesIO(base64.b64decode(person['photo']))))
+            self.photo.delete("all")
+            self.photo.create_image(100, 100, image=img)
+            self.photo.image = img
+        except:
+            self.photo.delete("all")
+            messagebox.showinfo(title="사진 미출력", message="본 데이터에는 사진 데이터가 없습니다.")
 
 FrameWork()
