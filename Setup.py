0a3ebd4d4dab8d207897528c29fcf20d25660e96
hyung tak Ju <juht97@naver.com>
2020-06-21 00:41:04 +0900

    setup ',' 추가해씀

--- Setup.py
+++ Setup.py
@@ -6,7 +6,7 @@ cMod = Extension('Spam', sources=['Spammodule.cpp'])
 setup(name='FindFor',
       version=1.0,
       ext_package='pkg',
-      py_modules=['DataBase' 'FrameWork', 'Gmail', 'Gmap', 'GraphData', 'Telegram', 'xmlControl', 'Setup'],
+      py_modules=['DataBase', 'FrameWork', 'Gmail', 'Gmap', 'GraphData', 'Telegram', 'xmlControl', 'Setup'],
       ext_modules=[cMod],
       data_files=[('',['GoogleMap.png']),('',['Telegram.png']),('',['Gmail.png'])]
       )
