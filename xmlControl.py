feee7b20740a1ac925b73caacceaf13d8ef52de0
asdssp <62279049+asdssp@users.noreply.github.com>
2020-06-21 00:55:06 +0900

    업데이트

--- xmlControl.py
+++ xmlControl.py
@@ -37,7 +37,7 @@ from xml.etree import ElementTree
 
 class xmlControl:
     def loadXmlFromOpenAPI(params):
-        client_id = "10000335"
+        client_id = ""
         client_secret = ""
         conn = http.client.HTTPConnection("safe182.go.kr")
         #conn.set_debuglevel(1) #debug mode �¦ㅼ��
